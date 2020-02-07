4b510c6544ab224e3514a082854e2e7bf4c5ffbd
Karthik Patlolla <Karthik_Patlolla@epam.com>
2020-02-07 17:52:01 +0530

    Add project files.

---
+++ UserRegistration/UserRegistrationForm.aspx
@@ -0,0 +1,113 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UserRegistrationForm.aspx.cs" Inherits="UserRegistration.UserRegistrationForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+<form id="form1" runat="server">  
+            <div  >  
+                   <fieldset id="User Form" style="width: 60%">  
+                    <legend>User Registration Form</legend>  
+                    <table>  
+                        <tr>  
+                            <td colspan="2">  
+                                <br /> </td>  
+                        </tr>  
+                        <tr>  
+                            <td>First Name:</td>  
+                            <td>
+                                <asp:TextBox ID="FirstNameTextBox" runat="server"></asp:TextBox>  
+                                </td>  
+                        </tr>  
+                        <tr>  
+                            <td>Last Name:</td>  
+                            <td>  
+                                 <asp:TextBox ID="LastNameTextBox" runat="server"></asp:TextBox>
+                            </td>  
+                        </tr>  
+                        <tr>  
+                             <td>Email Address:</td>  
+                            <td>  
+                                 <asp:TextBox ID="EmailTextBox" runat="server"></asp:TextBox>
+                            </td> 
+                        </tr>  
+                    </table> 
+                       <br />
+                       <fieldset id="Gender" style="width: 234px">  
+                           <legend>Choose Gender</legend>  
+                           <table>
+                        <tr>  
+                            <td colspan="2">  
+                            </td>  
+                        </tr>
+                              
+                                                       <tr>  
+                            <td>Male</td>  
+                            <td>
+                               <asp:RadioButton ID="MaleRadioButton" runat="server" GroupName="GenderRB" />
+                                </td>  
+                        </tr>
+                                                                                      <tr>  
+                            <td>Female</td>  
+                            <td>
+                               <asp:RadioButton ID="FemaleRadioButton" runat="server" GroupName="GenderRB" />
+                                </td>  
+                        </tr>
+
+                           </table>
+                           </fieldset>
+                 <br />
+                <fieldset id="Drop Down" style="width: 50%">  
+                    <legend>Choose Location</legend>  
+                    <table>  
+                        <tr>  
+                            <td colspan="2">  
+                                <br /> </td>  
+                        </tr>  
+                        <tr>  
+                            <td>Select Country:</td>  
+                            <td>  
+                                <asp:DropDownList ID="ddlCountry" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlCountry_SelectedIndexChanged"> </asp:DropDownList>  
+                            </td>  
+                        </tr>  
+                        <tr>  
+                            <td>Select State:</td>  
+                            <td>  
+                                <asp:DropDownList ID="ddlState" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlState_SelectedIndexChanged"> </asp:DropDownList>  
+                            </td>  
+                        </tr>  
+                        <tr>  
+                             <td>Select City:</td>  
+                            <td>  
+                                <asp:DropDownList ID="ddlCity" runat="server" ></asp:DropDownList>  
+                            </td> 
+                        </tr>  
+                    </table>  
+                </fieldset >  
+                       <br />
+                       <fieldset id="CheckBox" style="width: 50%">  
+                           <legend>Choose Stream</legend>  
+                           <table>
+                        <tr>  
+                            <td colspan="2">  
+                            </td>  
+                        </tr>
+                              
+                               <asp:CheckBoxList ID="StreamCheckBox" runat="server" ></asp:CheckBoxList>
+
+                           </table>
+                           </fieldset>
+    <br />
+    <br />
+    <asp:Calendar ID="Calendar" runat="server" onselectionchanged="Calendar_SelectionChanged"></asp:Calendar>
+    <br />
+    <asp:Button ID="Button" runat="server" Text="Submit"  OnClick="Button_Click" />
+                                        </fieldset >        
+            </div> 
+
+        </form>  
+</body>
+</html>
---
+++ UserRegistration/UserRegistrationForm.aspx.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UserRegistration
+{
+    public partial class UserRegistrationForm : System.Web.UI.Page
+    {
+        SqlConnection objSqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["myconnection"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (objSqlConnection.State == ConnectionState.Closed)
+            {
+                objSqlConnection.Open();
+            }
+            if (!IsPostBack)
+            {
+                ddlState.Items.Insert(0, new ListItem("--Select State--", "0"));
+                ddlCity.Items.Insert(0, new ListItem("--Select City--", "0"));
+                BindCountries();
+                Bind_CheckList();
+            }
+        }
+        protected void BindCountries()
+        {
+            try
+            {
+                SqlDataAdapter objSQLAdapter = new SqlDataAdapter("select * from Country", objSqlConnection);
+                DataSet objDataSet = new DataSet();
+                objSQLAdapter.Fill(objDataSet);
+                ddlCountry.DataSource = objDataSet;
+                ddlCountry.DataTextField = "CountryName";
+                ddlCountry.DataValueField = "CountryID";
+                ddlCountry.DataBind();
+                ddlCountry.Items.Insert(0, new ListItem("--Select Country--", "0"));
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Exception in Binding Country Dropdownlist : " + ex.Message.ToString());
+            }
+            finally
+            {
+                objSqlConnection.Close();
+            }
+        }
+        protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int CountryId = Convert.ToInt32(ddlCountry.SelectedValue);
+                SqlDataAdapter objSQLAdapter = new SqlDataAdapter("select * from State where CountryID=" + CountryId, objSqlConnection);
+                DataSet objDataSet = new DataSet();
+                objSQLAdapter.Fill(objDataSet);
+                ddlState.DataSource = objDataSet;
+                ddlState.DataTextField = "StateName";
+                ddlState.DataValueField = "StateID";
+                ddlState.DataBind();
+                ddlState.Items.Insert(0, new ListItem("--Select State--", "0"));
+                if (ddlState.SelectedValue == "0")
+                {
+                    ddlCity.Items.Clear();
+                    ddlCity.Items.Insert(0, new ListItem("--Select City--", "0"));
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Exception in Binding State Dropdownlist: " + ex.Message.ToString());
+            }
+            finally
+            {
+                objSqlConnection.Close();
+            }
+        }
+        protected void ddlState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int StateId = Convert.ToInt32(ddlState.SelectedValue);
+                SqlDataAdapter objSQLAdapter = new SqlDataAdapter("select * from City where StateID=" + StateId, objSqlConnection);
+                DataSet objDataSet = new DataSet();
+                objSQLAdapter.Fill(objDataSet);
+                ddlCity.DataSource = objDataSet;
+                ddlCity.DataTextField = "CityName";
+                ddlCity.DataValueField = "CityID";
+                ddlCity.DataBind();
+                ddlCity.Items.Insert(0, new ListItem("--Select City--", "0"));
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Exception in Binding City Dropdownlist: " + ex.Message.ToString());
+            }
+            finally
+            {
+                objSqlConnection.Close();
+            }
+        }
+        protected void Calendar_SelectionChanged(object sender, EventArgs e)
+        {
+            String DOB=  Calendar.SelectedDate.ToShortDateString();
+            
+        }
+        protected void Bind_CheckList() // Method for Binding The Checkbox List  
+        {
+            List<string> listContents = new List<string>();// Create a List of String Elements  
+            listContents.Add("Java");
+            listContents.Add(".NET");
+
+            StreamCheckBox.DataSource = listContents;//Set Datasource to CheckBox List  
+            StreamCheckBox.DataBind(); // Bind the checkboxList with String List.  
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand command;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                String sql = "";
+                String str = "";
+                for (int i = 0; i <= StreamCheckBox.Items.Count - 1; i++)
+                {
+                    if (StreamCheckBox.Items[i].Selected)
+                    {
+                        if (str == "")
+                        {
+                            str = StreamCheckBox.Items[i].Text;
+                        }
+                        else
+                        {
+                            str += " " + StreamCheckBox.Items[i].Text;
+                        }
+                    }
+                }
+                sql = "Insert into Users(FirstName,LastName,EmaiID,Location,Stream,DOB) values('" + FirstNameTextBox.Text + "','" + LastNameTextBox.Text + "','"+ EmailTextBox.Text + "','"+ ddlCity.SelectedItem+" "+ddlState.SelectedItem+" "+ddlCountry.SelectedItem+ "','"+ str + "','"+ Calendar.SelectedDate.ToShortDateString()+"')";
+                command = new SqlCommand(sql, objSqlConnection);
+                adapter.InsertCommand = new SqlCommand(sql, objSqlConnection);
+                adapter.InsertCommand.ExecuteNonQuery();
+                command.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Exception in insering Data: " + ex.Message.ToString());
+            }
+            finally
+            {
+                objSqlConnection.Close();
+            }
+
+        }
+    }
+}
